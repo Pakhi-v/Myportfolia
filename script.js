e02e14722bafde220f68573f145498ddd28a3248
pakhi <pakhivashis182001@gmail.com>
2023-01-09 16:55:13 +0530

    removal of image

--- script.js
+++ script.js
@@ -42,14 +42,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Graphic Desginer", "Blogger", "Coder"],
+        strings: ["Web Developer", "Graphic Desginer", "Blogger", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Graphic Desginer", "Blogger", "Coder"],
+        strings: ["Web Developer", "Graphic Desginer", "Blogger", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
